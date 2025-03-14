8c0cdf39e2da33ba71d40b993bdcea0eb6ec97f6
Cantero Martel Mariana Itzel <cantero.martel@gmail.com>
2025-03-13 21:04:03 -0600

    ejercicio1

--- ejercicios/ejercicio1.c
+++ ejercicios/ejercicio1.c
@@ -3,26 +3,32 @@
 
 int main() {
     int n;
+
     printf("Ingrese el tamaño del arreglo: ");
-    
+    scanf("%d", &n);
 
     int *arr;
+    // Reserva de memoria dinámicamente
+    arr = (int *)malloc(n * sizeof(int));
     if (arr == NULL) {
-        printf("Error: No se pudo asignar memoria.\n");
+        printf("Error al asignar memoria.\n");
         return 1;
     }
-
-    printf("Ingrese %d valores:\n", n);
+    
     for (int i = 0; i < n; i++) {
-        
+        printf("Ingrese el valor para arr[%d]: ", i);
+        scanf("%d", &arr[i]);
     }
 
-    printf("El arreglo es: ");
+    // Imprimir el arreglo
+    printf("Arreglo ingresado: ");
     for (int i = 0; i < n; i++) {
-        
+        printf("%d ", arr[i]);
     }
     printf("\n");
 
-    
+    // Liberar memoria
+    free(arr);
+
     return 0;
 }
